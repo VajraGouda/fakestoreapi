a80db4d43ed99151afdbbdedb0000760bc1ff0b4
VajraGouda <patil.aditya840@gmail.com>
2023-06-03 03:36:37 +0530

    'testing-change'

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
 
             <nav className="navbar navbar-expand-lg bg-white py-3 shadow-sm" >
                 <div className="container">
-                    <Link className="navbar-brand fw-bold fs-4" to="/">SHOPLANE</Link>
+                    <Link className="navbar-brand fw-bold fs-4" to="/"><span className="text-primary">SHOP</span>LANE</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
